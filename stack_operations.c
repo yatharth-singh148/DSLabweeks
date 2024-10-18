5d1b2f4d934f8741aa0570ac4d940586149938e0
yatharth-singh148 <yatharthsingh.cs23@bmsce.ac.in>
2024-10-18 15:50:49 +0530

    Update stack_operations.c

--- stack_operations.c
+++ stack_operations.c
@@ -1,88 +1,107 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<ctype.h>
-#include<string.h>
-#define MAX 100
-char stack[MAX];
-int top = -1;
-void push(char c)
-{
-    if (top == MAX - 1)
-    {
-        printf("Stack overflow\n");
-        return;
+#include <stdio.h>
+#include <stdlib.h>
+void push(int *arr, int *top, int size, int ele) {
+    if (*top != size - 1) {
+        arr[++(*top)] = ele;
+        printf("\nElement Pushed\n");
+    } else {
+        printf("\nStack is full.");
     }
-    stack[++top] = c;
 }
-char pop() 
-{
-    if (top == -1) 
-    {
-        printf("Stack underflow\n");
-        return -1;
+void pop(int *arr, int *top) {
+    if (*top != -1) {
+        printf("\nPopped element `%d`", arr[*top]);
+        arr[*top] = 0;
+        (*top)--;
+        printf("\nElement Popped\n");
+    } else {
+        printf("\nStack is Empty.");
     }
-    return stack[top--];
 }
-int precedence(char c)
-{
-    if (c == '^')
-        return 3;
-    else if (c == '*' || c == '/')
-        return 2;
-    else if (c == '+' || c == '-')
-        return 1;
-    else
-        return 0;
-}
-int isOperator(char c)
-{
-    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
-}
-void infixToPostfix(char* infix, char* postfix) 
-{
-    int i = 0, j = 0;
-    char c;
-    while (infix[i] != '\0')
-        {
-        if (isalnum(infix[i]))
-        {
-            postfix[j++] = infix[i];
-        }
-        else if (infix[i] == '(')
-        {
-            push(infix[i]);
-        }
-        else if (infix[i] == ')')
-        {
-            while (top != -1 && stack[top] != '(')
-            {
-                postfix[j++] = pop();
-            }
-            pop();
-        }
-
-        else if (isOperator(infix[i]))
-        {
-            while (top != -1 && precedence(stack[top]) >= precedence(infix[i]))
-            {
-                postfix[j++] = pop();
-            }
-            push(infix[i]);
-        }
-        i++;
+void display(int *arr, int top) {
+    printf("\n");
+    for (int i = top; i >= 0; i--) {
+        printf("%d\t", arr[i]);
     }
-    while (top != -1)
-    {
-        postfix[j++] = pop();
-    }
-    postfix[j] = '\0';
 }
-int main()
-{
-    char infix[MAX], postfix[MAX];
-    printf("Enter an infix expression: ");
-    scanf("%s", infix);
-    infixToPostfix(infix, postfix);
-    printf("Postfix expression: %s\n", postfix);
+int main() {
+    int size;
+    printf("Enter size of stack: ");
+    scanf("%d", &size);
+    int arr[100];
+    int top = -1;
+    int ch = 1;
+    int e;
+    while (ch < 4 && ch > 0) {
+        printf("\nPick option:\n1) Push\n2) Pop\n3) Display\n4) Exit\n");
+        scanf("%d", &ch);
+        switch (ch) {
+            case 1:
+                printf("Enter element to push:\n");
+                scanf("%d", &e);
+                push(arr, &top, size, e);
+                break;
+            case 2:
+                pop(arr, &top);
+                break;
+            case 3:
+                display(arr, top);
+                break;
+            case 4:
+                printf("\nClosing...");
+                break;
+            default:
+                printf("\nEnter valid option!");
+        }
+    }
     return 0;
 }
+//below using global var
+/*#include <stdio.h>
+#include <stdlib.h>
+int arr[100],size,top;
+int push(int ele)
+{
+    if(top!=size-1)
+    {
+        arr[top+1]=ele;
+        top++;
+        printf("\nElement Pushed\n");
+    }
+    else if(top==size-1)
+    {
+        printf("\nStack is full.");
+    }
+}
+int pop()
+{
+        if(top!=-1)
+    {
+        printf("\nPopped element `%d`",arr[top]);
+        arr[top+1]=0;
+        top--;
+        printf("\nElement Popped\n");
+    }
+    else if(top==-1)
+}
+void main()
+{
+    size=0;
+    printf("Enter size of stack: ");
+    scanf("%d",&size);
+    top=-1;
+    int ch=1;
+    int e;
+    while(ch<4 && ch>0)
+    {
+        printf("\nPick option:\n1) Push\n2) Pop\n3) Display\n4) Exit\n");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+            case 1: printf("Enter element to push:\n"); scanf("%d",&e); push(e); break;
+            case 2: pop(); break;
+            case 3: display(); break;
+            case 4: printf("\nClosing..."); break;
+            default: printf("\nEnter valid option!");
+        }
+    }

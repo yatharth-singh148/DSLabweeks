4d7ff7ccc3b1e7b418fb86af8f916f79fbae8018
yatharth-singh148 <yatharthsingh.cs23@bmsce.ac.in>
2024-11-15 15:33:11 +0530

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node {
+struct Node 
+{
     int data;
     struct Node* next;
 };
 
 struct Node* head = NULL;
-void insertFront(int data) {
+void insertFront(int data) 
+{
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = data;
     newNode->next = head;
@@ -19,25 +21,29 @@ void insertEnd(int data) {
     newNode->data = data;
     newNode->next = NULL;
 
-    if (head == NULL) {
+    if (head == NULL) 
+    {
         head = newNode;
+        //printf("h");
         return;
     }
-
     struct Node* temp = head;
-    while (temp->next != NULL) {
+    while (temp->next != NULL) 
+    {
         temp = temp->next;
     }
     temp->next = newNode;
 }
 
-void insertAfter(int data, int pos) {
+void insertAfter(int data, int pos) 
+{
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = data;
     newNode->next = NULL;
 
     struct Node* temp = head;
-    for (int i = 0; i < pos - 1; i++) {
+    for (int i = 0; i < pos - 1; i++) 
+    {
         if (temp == NULL) {
             printf("Position out of range\n");
             return;
@@ -49,15 +55,18 @@ void insertAfter(int data, int pos) {
     temp->next = newNode;
 }
 
-void deleteNode(int value) {
+void deleteNode(int value) 
+{
     struct Node* temp = head;
     struct Node* prev = NULL;
-    if (temp != NULL && temp->data == value) {
+    if (temp != NULL && temp->data == value) 
+    {
         head = temp->next;
         free(temp);
         return;
     }
-    while (temp != NULL && temp->data != value) {
+    while (temp != NULL && temp->data != value) 
+    {
         prev = temp;
         temp = temp->next;
     }
@@ -68,7 +77,8 @@ void deleteNode(int value) {
     prev->next = temp->next;
     free(temp);
 }
-void displayList() {
+void displayList() 
+{
     struct Node* temp = head;
     if (temp == NULL) {
         printf("List is empty\n");
@@ -82,21 +92,22 @@ void displayList() {
     printf("NULL\n");
 }
 
-int main() {
+int main() 
+{
     int choice, data, pos;
-
-    while (1) {
-        printf("\nMenu:\n");
-        printf("1. Insert at front\n");
-        printf("2. Insert at end\n");
-        printf("3. Insert after position\n");
-        printf("4. Delete by value\n");
-        printf("5. Display list\n");
-        printf("6. Exit\n");
+    while (1) 
+    {
+        printf("\n----------[~ LINKED LIST OPERATIONS ~]----------\n");
+        printf("1 -> Insert at front\n");
+        printf("2 -> Insert at end\n");
+        printf("3 -> Insert after position\n");
+        printf("4 -> Delete by value\n");
+        printf("5 -> Display list\n");
+        printf("6 -> Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
-
-        switch (choice) {
+        switch (choice) 
+        {
             case 1:
                 printf("Enter data to insert at front: ");
                 scanf("%d", &data);
